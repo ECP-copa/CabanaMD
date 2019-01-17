9bdf9748bba92643d9f3240c20bf405005203a66
Sam Reeve <reeve5@llnl.gov>
2019-01-17 00:17:04 -0800

    Add Cabana typedefs; remove existing data structures

--- src/types.h
+++ src/types.h
@@ -50,6 +50,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 #include<Kokkos_Core.hpp>
+#include<Cabana_Core.hpp>
+#define VECLEN 16
 
 // Module Types etc
 // Units to be used
@@ -93,99 +95,28 @@ enum {NEIGH_CABANA_VERLET};
 #define T_F_FLOAT T_FLOAT
 #endif
 
-// Define Kokkos View Types
-typedef Kokkos::View<T_X_FLOAT*[3],Kokkos::LayoutRight>       t_x;          // Positions
-typedef Kokkos::View<const T_X_FLOAT*[3],Kokkos::LayoutRight> t_x_const;    // Positions
-typedef Kokkos::View<const T_X_FLOAT*[3],Kokkos::LayoutRight,
-    Kokkos::MemoryTraits<Kokkos::RandomAccess>> t_x_const_rnd;    // Positions
-typedef Kokkos::View<T_V_FLOAT*[3]>       t_v;          // Velocities
-typedef Kokkos::View<T_F_FLOAT*[3]>       t_f;          // Force
-typedef Kokkos::View<T_F_FLOAT*[3],
-#ifdef KOKKOS_ENABLE_SERIAL
-    Kokkos::MemoryTraits<std::is_same<Kokkos::DefaultExecutionSpace,Kokkos::Serial>::value?0:Kokkos::Atomic>>
-#else
-    Kokkos::MemoryTraits<Kokkos::Atomic>>
-#endif
-        t_f_atomic;   // Force
-typedef Kokkos::View<const T_F_FLOAT*[3]> t_f_const;    // Force
-
-typedef Kokkos::View<int*>                t_type;       // Type (int is enough as type)
-typedef Kokkos::View<const int*>          t_type_const; // Type (int is enough as type)
-typedef Kokkos::View<const int*,
-    Kokkos::MemoryTraits<Kokkos::RandomAccess>> t_type_const_rnd; // Type (int is enough as type)
-typedef Kokkos::View<T_INT*>              t_id;         // ID
-typedef Kokkos::View<const T_INT*>        t_id_const;   // ID
-typedef Kokkos::View<T_FLOAT*>            t_q;          // Charge
-typedef Kokkos::View<const T_FLOAT*>      t_q_const;    // Charge
-
 typedef Kokkos::View<T_V_FLOAT*>          t_mass;       // Mass
 typedef Kokkos::View<const T_V_FLOAT*>    t_mass_const; // Mass
 
-typedef Kokkos::DefaultExecutionSpace::memory_space t_neigh_mem_space;
 
-template<class Scalar>
-struct t_scalar3 {
-  Scalar x,y,z;
+//Cabana
+using t_tuple = Cabana::MemberTypes<T_FLOAT[3], T_FLOAT[3], T_FLOAT[3],
+                                    T_INT, T_INT, T_FLOAT>;
+enum TypeNames { Positions = 0, Velocities = 1, Forces = 2,
+                 Types = 3, IDs = 4, Charges = 5 };
 
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3() {
-    x = 0; y = 0; z = 0;
-  }
+using MemorySpace = Cabana::HostSpace;
+using MemoryAccess = Cabana::DefaultAccessMemory;
+using AtomicAccess = Cabana::AtomicAccessMemory;
+using AoSoA = Cabana::AoSoA<t_tuple,MemorySpace,VECLEN>;
+using t_particle = Cabana::Tuple<t_tuple>;
+using t_slice_x = Cabana::Slice<T_FLOAT[3],MemorySpace,MemoryAccess,VECLEN>;
+using t_slice_x_atomic = Cabana::Slice<T_FLOAT[3],MemorySpace,AtomicAccess,VECLEN>;
+using t_slice_int = Cabana::Slice<T_INT,MemorySpace,MemoryAccess,VECLEN>;
+using t_slice_fl = Cabana::Slice<T_FLOAT,MemorySpace,MemoryAccess,VECLEN>;
 
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3(const t_scalar3& rhs) {
-    x = rhs.x; y = rhs.y; z = rhs.z;
-  }
+using t_verletlist_full = Cabana::VerletList<MemorySpace,Cabana::FullNeighborTag>;
+using t_verletlist_half = Cabana::VerletList<MemorySpace,Cabana::HalfNeighborTag>;
 
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3(const Scalar& x_, const Scalar& y_, const Scalar& z_ ) {
-    x = x_; y = y_; z = z_;
-  }
-
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3 operator= (const t_scalar3& rhs) {
-    x = rhs.x; y = rhs.y; z = rhs.z;
-    return *this;
-  }
-
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3 operator= (const volatile t_scalar3& rhs) {
-    x = rhs.x; y = rhs.y; z = rhs.z;
-    return *this;
-  }
-
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3 operator+= (const t_scalar3& rhs) {
-    x += rhs.x; y += rhs.y; z += rhs.z;
-    return *this;
-  }
-
-  KOKKOS_FORCEINLINE_FUNCTION
-  t_scalar3 operator+= (const volatile t_scalar3& rhs) volatile {
-    x += rhs.x; y += rhs.y; z += rhs.z;
-    return *this;
-  }
-};
-
-template<class Scalar>
-KOKKOS_FORCEINLINE_FUNCTION
-t_scalar3<Scalar> operator +
-  (const t_scalar3<Scalar>& a, const t_scalar3<Scalar>& b) {
-  return t_scalar3<Scalar>(a.x+b.x,a.y+b.y,a.z+b.z);
-}
-
-template<class Scalar>
-KOKKOS_FORCEINLINE_FUNCTION
-t_scalar3<Scalar> operator *
-  (const t_scalar3<Scalar>& a, const Scalar& b) {
-  return t_scalar3<Scalar>(a.x*b,a.y*b,a.z*b);
-}
-
-template<class Scalar>
-KOKKOS_FORCEINLINE_FUNCTION
-t_scalar3<Scalar> operator *
-  (const Scalar& b, const t_scalar3<Scalar>& a) {
-  return t_scalar3<Scalar>(a.x*b,a.y*b,a.z*b);
-}
 #endif
 
