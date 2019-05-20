e9c9b96891052163eda6d7def45fba86a5b2a041
Sam Reeve <reeve5@llnl.gov>
2019-05-20 16:40:31 -0700

    Use Cabana cmake defines (ECP-copa/Cabana#116)

--- src/main.cpp
+++ src/main.cpp
@@ -52,13 +52,13 @@
 // CabanaMD can be used as a library
 // This main file is simply a driver
 
-#ifdef CabanaMD_ENABLE_MPI
+#ifdef Cabana_ENABLE_MPI
 #include "mpi.h"
 #endif
 
 int main(int argc, char* argv[]) {
 
-   #ifdef CabanaMD_ENABLE_MPI
+   #ifdef Cabana_ENABLE_MPI
    MPI_Init(&argc,&argv);
    #endif
 
@@ -75,7 +75,7 @@ int main(int argc, char* argv[]) {
 
    cabanamd.shutdown();
 
-   #ifdef CabanaMD_ENABLE_MPI
+   #ifdef Cabana_ENABLE_MPI
    MPI_Finalize();
    #endif
 }
