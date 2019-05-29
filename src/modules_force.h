450d98e04c8677a97ea7c7e768634f849cf48adc
Sam Reeve <reeve5@llnl.gov>
2019-05-29 12:12:28 -0700

    Update with current force styles

--- src/modules_force.h
+++ src/modules_force.h
@@ -48,7 +48,5 @@
 //************************************************************************
 
 // Include Module header files for force
-#include <force_lj_cell.h>
-#include <force_lj_neigh.h>
-#include <force_lj_idial_neigh.h>
-#include <force_snap_neigh.h>
+#include <force_lj_cabana_neigh.h>
+//#include <force_snap_cabana_neigh.h>
