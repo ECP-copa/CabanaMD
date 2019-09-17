81e1c16380f63376ea329ed0345934e06352adba
Sam Reeve <reeve5@llnl.gov>
2019-09-17 15:49:48 -0700

    Add Cabana license to nnp files

--- src/force_types/nnp_cutoff.h
+++ src/force_types/nnp_cutoff.h
@@ -1,3 +1,14 @@
+/****************************************************************************
+ * Copyright (c) 2018-2019 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
 // n2p2 - A neural network potential package
 // Copyright (C) 2018 Andreas Singraber (University of Vienna)
 //
