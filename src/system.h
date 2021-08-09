fdabef6cb7ea245c1d519a5aa4a8ba61ee4b41eb
Stephan Schulz <stephan.schulz-x2q@ruhr-uni-bochum.de>
2021-08-09 12:10:11 +0200

    use Cajita grid to balance

--- src/system.h
+++ src/system.h
@@ -56,7 +56,10 @@
 #include <types.h>
 
 #include <algorithm>
+#include <cmath>
+#include <limits>
 #include <memory>
+#include <stdexcept>
 #include <string>
 
 template <class t_device>
@@ -84,6 +87,8 @@ class SystemCommon
 
     // Simulation total domain
     T_X_FLOAT global_mesh_x, global_mesh_y, global_mesh_z;
+    T_X_FLOAT grid_cell_size;
+    int grid_num_cells;
 
     // Simulation sub domain (single MPI rank)
     T_X_FLOAT local_mesh_x, local_mesh_y, local_mesh_z;
@@ -94,6 +99,8 @@ class SystemCommon
     T_X_FLOAT halo_width;
     std::shared_ptr<Cajita::LocalGrid<Cajita::UniformMesh<T_X_FLOAT>>>
         local_grid;
+    std::shared_ptr<Cajita::GlobalGrid<Cajita::UniformMesh<T_X_FLOAT>>>
+        global_grid;
 
     // Only needed for current comm
     std::array<int, 3> ranks_per_dim;
@@ -113,6 +120,9 @@ class SystemCommon
 
         mass = t_mass( "System::mass", ntypes );
 
+        // todo(sschulz): Choose a suitably small grid cell automatically
+        grid_cell_size = 0.0;
+        grid_num_cells = 2000;
         global_mesh_x = global_mesh_y = global_mesh_z = 0.0;
         local_mesh_lo_x = local_mesh_lo_y = local_mesh_lo_z = 0.0;
         local_mesh_hi_x = local_mesh_hi_y = local_mesh_hi_z = 0.0;
@@ -144,9 +154,21 @@ class SystemCommon
         Cajita::UniformDimPartitioner partitioner;
         ranks_per_dim = partitioner.ranksPerDimension( MPI_COMM_WORLD, {} );
 
+        // todo(sschulz): Generalize, so non cubic dimensions are allowed
+        grid_cell_size = ( high_corner[0] - low_corner[0] ) / grid_num_cells;
+        if ( std::abs( ( high_corner[0] - low_corner[0] ) -
+                       ( high_corner[1] - low_corner[1] ) ) >
+             T_X_FLOAT( 100.0 ) * std::numeric_limits<T_X_FLOAT>::epsilon() )
+            throw std::logic_error( "Dimensions must be cubic" );
+        else if ( std::abs( ( high_corner[1] - low_corner[1] ) -
+                            ( high_corner[2] - low_corner[2] ) ) >
+                  T_X_FLOAT( 100.0 ) *
+                      std::numeric_limits<T_X_FLOAT>::epsilon() )
+            throw std::logic_error( "Dimensions must be cubic" );
+
         // Create global mesh of MPI partitions.
         auto global_mesh = Cajita::createUniformGlobalMesh(
-            low_corner, high_corner, ranks_per_dim );
+            low_corner, high_corner, grid_cell_size );
 
         global_mesh_x = global_mesh->extent( 0 );
         global_mesh_y = global_mesh->extent( 1 );
@@ -154,8 +176,8 @@ class SystemCommon
 
         // Create the global grid.
         std::array<bool, 3> is_periodic = { true, true, true };
-        auto global_grid = Cajita::createGlobalGrid(
-            MPI_COMM_WORLD, global_mesh, is_periodic, partitioner );
+        global_grid = Cajita::createGlobalGrid( MPI_COMM_WORLD, global_mesh,
+                                                is_periodic, partitioner );
 
         for ( int d = 0; d < 3; d++ )
         {
@@ -163,45 +185,27 @@ class SystemCommon
         }
 
         // Create a local mesh
-        local_grid = Cajita::createLocalGrid( global_grid, 0 );
-        auto local_mesh = Cajita::createLocalMesh<t_device>( *local_grid );
-
-        local_mesh_lo_x = local_mesh.lowCorner( Cajita::Own(), 0 );
-        local_mesh_lo_y = local_mesh.lowCorner( Cajita::Own(), 1 );
-        local_mesh_lo_z = local_mesh.lowCorner( Cajita::Own(), 2 );
-        local_mesh_hi_x = local_mesh.highCorner( Cajita::Own(), 0 );
-        local_mesh_hi_y = local_mesh.highCorner( Cajita::Own(), 1 );
-        local_mesh_hi_z = local_mesh.highCorner( Cajita::Own(), 2 );
-        ghost_mesh_lo_x = local_mesh_lo_x - halo_width;
-        ghost_mesh_lo_y = local_mesh_lo_y - halo_width;
-        ghost_mesh_lo_z = local_mesh_lo_z - halo_width;
-        ghost_mesh_hi_x = local_mesh_hi_x + halo_width;
-        ghost_mesh_hi_y = local_mesh_hi_y + halo_width;
-        ghost_mesh_hi_z = local_mesh_hi_z + halo_width;
-        local_mesh_x = local_mesh.extent( Cajita::Own(), 0 );
-        local_mesh_y = local_mesh.extent( Cajita::Own(), 1 );
-        local_mesh_z = local_mesh.extent( Cajita::Own(), 2 );
+        int halo_width = std::ceil( ghost_cutoff / grid_cell_size );
+        local_grid = Cajita::createLocalGrid( global_grid, halo_width );
+        // Update local_mesh_* and ghost_mesh_* info
+        update_mesh_info();
     }
     // low_corner and high_corner are local corners, not global as in
     // create_domain!
     void update_domain( std::array<double, 3> low_corner,
                         std::array<double, 3> high_corner )
     {
-        local_mesh_lo_x = low_corner[0];
-        local_mesh_lo_y = low_corner[1];
-        local_mesh_lo_z = low_corner[2];
-        local_mesh_hi_x = high_corner[0];
-        local_mesh_hi_y = high_corner[1];
-        local_mesh_hi_z = high_corner[2];
-        local_mesh_x = local_mesh_hi_x - local_mesh_lo_x;
-        local_mesh_y = local_mesh_hi_y - local_mesh_lo_y;
-        local_mesh_z = local_mesh_hi_z - local_mesh_lo_z;
-        ghost_mesh_lo_x = local_mesh_lo_x - halo_width;
-        ghost_mesh_lo_y = local_mesh_lo_y - halo_width;
-        ghost_mesh_lo_z = local_mesh_lo_z - halo_width;
-        ghost_mesh_hi_x = local_mesh_hi_x + halo_width;
-        ghost_mesh_hi_y = local_mesh_hi_y + halo_width;
-        ghost_mesh_hi_z = local_mesh_hi_z + halo_width;
+        // Calculate new local grid offset
+        std::array<int, 3> cell_index_lo, cell_index_hi;
+        for ( std::size_t d = 0; d < 3; ++d )
+            cell_index_lo[d] = std::rint( low_corner[d] / grid_cell_size );
+        for ( std::size_t d = 0; d < 3; ++d )
+            cell_index_hi[d] = std::rint( high_corner[d] / grid_cell_size );
+        std::array<int, 3> num_cell;
+        for ( std::size_t d = 0; d < 3; ++d )
+            num_cell[d] = cell_index_hi[d] - cell_index_lo[d];
+        global_grid->setNumCellAndOffset( num_cell, cell_index_lo );
+        update_mesh_info();
     }
 
     void slice_all()
@@ -245,6 +249,29 @@ class SystemCommon
     migrate( std::shared_ptr<Cabana::Distributor<t_device>> distributor ) = 0;
     virtual void gather( std::shared_ptr<Cabana::Halo<t_device>> halo ) = 0;
     virtual const char *name() { return "SystemNone"; }
+
+  private:
+    // Update local_mesh_* and ghost_mesh* info from global grid
+    void update_mesh_info()
+    {
+        auto local_mesh = Cajita::createLocalMesh<t_device>( *local_grid );
+
+        local_mesh_lo_x = local_mesh.lowCorner( Cajita::Own(), 0 );
+        local_mesh_lo_y = local_mesh.lowCorner( Cajita::Own(), 1 );
+        local_mesh_lo_z = local_mesh.lowCorner( Cajita::Own(), 2 );
+        local_mesh_hi_x = local_mesh.highCorner( Cajita::Own(), 0 );
+        local_mesh_hi_y = local_mesh.highCorner( Cajita::Own(), 1 );
+        local_mesh_hi_z = local_mesh.highCorner( Cajita::Own(), 2 );
+        ghost_mesh_lo_x = local_mesh.lowCorner( Cajita::Ghost(), 0 );
+        ghost_mesh_lo_y = local_mesh.lowCorner( Cajita::Ghost(), 1 );
+        ghost_mesh_lo_z = local_mesh.lowCorner( Cajita::Ghost(), 2 );
+        ghost_mesh_hi_x = local_mesh.highCorner( Cajita::Ghost(), 0 );
+        ghost_mesh_hi_y = local_mesh.highCorner( Cajita::Ghost(), 1 );
+        ghost_mesh_hi_z = local_mesh.highCorner( Cajita::Ghost(), 2 );
+        local_mesh_x = local_mesh.extent( Cajita::Own(), 0 );
+        local_mesh_y = local_mesh.extent( Cajita::Own(), 1 );
+        local_mesh_z = local_mesh.extent( Cajita::Own(), 2 );
+    }
 };
 
 template <class t_device, int layout>
